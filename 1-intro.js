1d7891d85814d2c4a5cdbcfdab519667a4c84ed9
Bhargav Vandavasi <vandavasi.bhargav@gmail.com>
2022-08-11 02:02:28 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 10;
+
+if (amount < 10) {
+  console.log("given number is smaller than ten");
+} else {
+  console.log("given number is greater than ten");
+}
+
+console.log("Hey it my first node app!!!");
---
+++ 7-mind-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 12;
+
+function addition() {
+  console.log(`addition of two numbers : ${num1 + num2}`);
+}
+
+addition();
